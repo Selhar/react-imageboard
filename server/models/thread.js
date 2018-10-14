a8dad150646662234bbf59604fe6c21f6a1ea396
selhar <selhar@protonmail.com>
2018-10-14 16:29:56 -0300

    update css and default password

--- server/models/thread.js
+++ server/models/thread.js
@@ -1,17 +1,18 @@
 const mongoose = require('mongoose');
 const ObjectId = require('mongodb').ObjectID;
 const Schema = mongoose.Schema;
+const cuid = require('cuid');
 
 const Thread = new Schema({
   title:          { type: String },
   text:           { type: String, required: true },
   creation_date:  { type: Date, default: Date.now, required: true },
-  password:       { type: String, default: "wrathofthegods" },
+  password:       { type: String, default: cuid() },
   isReported:     { type: Boolean, default: false },
   replies: [{
     _id:        { type: Schema.Types.ObjectId, default: new ObjectId() },
     text:       { type: String, required: true },
-    password:   { type: String, default: "wrathofthegods" },
+    password:   { type: String, default: cuid() },
     isReported: { type: Boolean, default: false }
   }]
 });
